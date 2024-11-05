16650755647aa3c1049c757c1023b6af8d095623
Olavo <mr.sadstar@gmail.com>
2024-11-05 18:29:56 -0300

    Add files via upload

---
+++ Aula_02_29.10.28/cadastrar_marcas.php
@@ -0,0 +1,29 @@
+<h1>cadastro</h1>
+<form action="?page-salvar_marcas" method="post">
+<input type="hidden" name="acao" value="cadastrar">
+
+<div class="card" style="width:30rem">
+    <div>
+        <div class="container mb-3">
+            <label class="mt-3">Marca</label>
+            <input type="text" name="marca" class="form-control" placeholder="digite nome da marca">
+        </div>
+        <div class="container mb-3">
+            <label class="mt-3">Ano</label>
+            <input type="text" name="marca" class="form-control" placeholder="digite nome da marca">
+        </div>
+        <div class="container mb-3">
+            <label class="mt-3">modelo</label>
+            <input type="text" name="marca" class="form-control" placeholder="digite nome da marca">
+        </div>
+        <div class="container mb-3">
+            <label class="mt-3">Placa</label>
+            <input type="text" name="marca" class="form-control" placeholder="digite nome da marca">
+        </div>
+        
+        <div>
+            <button type="submit" class="btn btn-primary"></button>
+        </div>
+    </div>
+</div>
+</form>
